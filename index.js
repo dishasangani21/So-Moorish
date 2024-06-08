dd881103b1dea01f620352abd8187d2a2e410f88
dishasanganu21 <dishasangani2106@gmail.com>
2024-06-08 18:01:54 +0530

    firest commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function more2() {
+    document.getElementById("more2").innerHTML="Wonderful taste & it may sound crazy but its a very chocolaty chocolate. It has an unique taste, sweet and just melts in your mouth when you give just a single bite. We love Dark Chocolates products and fall in love with yummy taste. ";
+
+}
+
+function more3(){
+    document.getElementById("more3").innerHTML="Raspberry popcorn peanut brittle marshmallows-caramel chocolate has great uses with high health benefits. With the goodness of vitamins and minerals Along with taste its very healthy for my kid.  "
+    document.getElementById("reviewbox").style={}
+}
+
+
+function more4(){
+    document.getElementById("more4").innerHTML="This pie is by far my favorite. Delicious, texture and flavour. Your simple chocolate almond made gourmet. Wasn't what I expect but still nice "
+}
+function more5(){
+    document.getElementById("more5").innerHTML="I tried Fruit and Nut - Caramel Chocolate for the first time, and I have to say, it's my new favorite chocolate flavour. The combination of nuts, dried fruit, and caramel is perfect.   "
+}
+function more6(){
+    document.getElementById("more6").innerHTML="Enjoy the combination of flavours, and the texture of the macadamia nuts adds a nice crunch. If you enjoy the taste of dark chocolates and are looking for a unique and flavorful chocolate treat, Raspberry Dusted Macadamia - Dark Chocolate may be worth a t  "
+}
